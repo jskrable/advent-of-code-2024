75c11a97a6a2635ab4228dc2752dc0e7e5ef0e02
Jack Skrable <jack.skrable@bybeam.co>
2024-12-06 16:12:37 -0500

    day1
    solutions 1 & 2

---
+++ day1/solution.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"math"
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
+func getLists() ([]int64, []int64) {
+	ex, _ := os.Executable()
+	absPath, _ := filepath.Abs(filepath.Join(filepath.Base(ex), "./input.txt"))
+	file, _ := os.Open(absPath)
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	reader.Comma = ','
+	records, _ := reader.ReadAll()
+
+	var a, b []int64
+	for _, record := range records {
+		if len(record) != 2 {
+			break
+		}
+		forA, _ := strconv.ParseInt(record[0], 0, 64)
+		forB, _ := strconv.ParseInt(record[1], 0, 64)
+
+		a = append(a, forA)
+		b = append(b, forB)
+	}
+
+	return a, b
+}
+
+func remove(s []int64, i int) []int64 {
+	ret := make([]int64, 0)
+	ret = append(ret, s[:i]...)
+	return append(ret, s[i+1:]...)
+}
+
+func findMinIndex(s []int64) int {
+	index := 0
+	min := s[index]
+	for i, num := range s {
+		if num < min {
+			index = i
+			min = s[index]
+		}
+	}
+	return index
+}
+
+func calcTotalDistance(a []int64, b []int64) int64 {
+	var dist int64
+	size := len(a)
+	for i := 0; i < size; i++ {
+		minA := findMinIndex(a)
+		minB := findMinIndex(b)
+		dist += int64(math.Abs(float64(a[minA] - b[minB])))
+		a = remove(a, minA)
+		b = remove(b, minB)
+	}
+	return dist
+}
+
+func getAppearanceCount(s []int64, val int64) int {
+	count := 0
+	for _, num := range s {
+		if num == val {
+			count++
+		}
+	}
+	return count
+}
+
+func getSimilarity(a []int64, b []int64) int64 {
+	var similarity int64
+	size := len(a)
+	for i := 0; i < size; i++ {
+		value := a[i]
+		appearances := getAppearanceCount(b, value)
+		similarity += value * int64(appearances)
+	}
+	return similarity
+}
+
+func main() {
+	a, b := getLists()
+	distance := calcTotalDistance(a, b)
+	similarity := getSimilarity(a, b)
+	fmt.Println("total distance: ", distance)
+	fmt.Println("similarity: ", similarity)
+}
